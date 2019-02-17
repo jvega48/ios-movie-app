369480a954ca51ac88295bff5d1dc7cd52981353
jvega48 <jvega4849@gmail.com>
2019-02-16 21:52:05 -0800

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Jose Vega on 2/16/19.
+//  Copyright © 2019 Jose Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
