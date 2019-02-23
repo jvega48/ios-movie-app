920fce5be21c7790e4c7d39c7bd189338582c09e
jvega48 <jvega4849@gmail.com>
2019-02-23 02:05:28 -0800

    async call faster data retrival

--- Movie App/ViewController.swift
+++ Movie App/ViewController.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+//    struct ResponseData: Codable {
+//        let data: [MoviesItem]
+//    }
+//
+//    struct MoviesItem: Codable {
+//        let page_: Int
+//        let totalResults: Int
+//        let totalRages: Int
+//        let results_total: [Results]
+//
+//        enum CodingKeys: String, CodingKey {
+//            case page_: "page"
+//            case totalResults: "total_results"
+//            case totalRanges: "total_pages"
+//            case results_total:"results"
+//        }
+//    }
+//
+//    struct Results: Codable {
+//
+//    }
     
     @IBOutlet weak var pgrs: UIActivityIndicatorView!
     @IBOutlet weak var apiData: UILabel!
     @IBOutlet weak var searchLabel: UILabel!
+    @IBOutlet weak var artistSearch: UITextField!
     @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var imagePlaceholder: UIImageView!
     @IBAction func imagePicker(_ sender: UITapGestureRecognizer) {
@@ -36,46 +58,52 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         apiData.isHidden = true
         pgrs.isHidden = true
         searchField.delegate = self
+        artistSearch.delegate = self
     }
     @IBAction func searchTrigger(_ sender: UIButton) {
         NSLog("Running the search trigger")
-        searchLabel.text = "Clicking"
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
         pgrs.startAnimating()
         pgrs.isHidden = false;
         apiData.isHidden = true
         NSLog("Running the search button trigger")
-        searchLabel.text = "Clicking"
-        
-        let API_URL = URL(string: "https://api.lyrics.ovh/v1/Drake/Fake%20Love")!
+        let API_URL = URL(string: "https://api.themoviedb.org/3/search/movie?api_key=???8&language=en-US&query=Star%20Wars&page=1&include_adult=false")!
+        var request = URLRequest(url: API_URL)
         NSLog("Checking the api call \(API_URL)")
-        let task = session.dataTask(with: API_URL) {(data, responds, error) in
+        let task = session.dataTask(with: request) {(data, responds, error) in
             print("In the session")
             let status = (responds as! HTTPURLResponse).statusCode
             print(status)
-            guard let a = data else {return}
-            do{
+            
+            if let response = responds {
+                print(responds)
+            
+                if let data = data, let body = String(data: data, encoding: .utf8){
+                    DispatchQueue.main.async{
+                        self.apiData.text = "\(body)"
+                        self.pgrs.stopAnimating()
+                        self.pgrs.isHidden = true
+                        self.apiData.isHidden = false
+                    }
+
+                }
                 
-                let str = try JSONSerialization.jsonObject(with: a, options: [])
-                NSLog("\(str)")
-                self.apiData.text = "\(str)"
-                self.pgrs.stopAnimating()
-                self.pgrs.isHidden = true
-                self.apiData.isHidden = false
-                
-            }catch{
+            } else {
                 NSLog("Error on the api request")
+
             }
-            
-            
         }
         
         task.resume()
     }
     
+    @IBAction func clearSearch(_ sender: UIButton) {
+        apiData.text = " "
+    }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchField.resignFirstResponder()
+        artistSearch.resignFirstResponder()
         return true
     }
     
